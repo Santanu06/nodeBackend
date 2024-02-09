e105e2978abe12902d57da600ef244c3f3c10421
Santanu Kumar Sahu <santanu06.1994@gmail.com>
2024-02-10 00:04:11 +0530

    Api Error and response setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js"
+
+const port = process.env.PORT || 8080
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log(`ERR:`,error);
+        throw error
+    })
+    app.listen(process.env.PORT , ()=>{
+        console.log(`Server is running at port: ${port} `);
+    })
+    
+})
+.catch((error)=>{
+    console.log(`Failed to connect MongoDB!!!`);
+})
