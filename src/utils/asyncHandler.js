b94a1764cb915b2a44e6d291da8ea36f6d4fc428
Santanu Kumar Sahu <santanu06.1994@gmail.com>
2024-02-15 23:25:02 +0530

    Created controller, middleware, route file

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,10 @@
 const asyncHandler = (fn)=>{
-    (req, res, next)=>{
+   return (req, res, next)=>{
         Promise.resolve(fn(req, res, next)).catch((err)=>next())
     }
 }
 
+export {asyncHandler}
 
 // const asyncHandler = (fn) => async (req, res, next) =>{
 //     try {
